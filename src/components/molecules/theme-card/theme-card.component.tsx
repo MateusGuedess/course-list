fe009fb6b7360a2728b88fbfe9990d542dd3a538
Mateus Guedes <mxteusgued@gmail.com>
2024-09-19 02:24:31 -0300

    feature: add data to cards

--- src/components/molecules/theme-card/theme-card.component.tsx
+++ src/components/molecules/theme-card/theme-card.component.tsx
@@ -6,11 +6,13 @@ import {
     Flex,
     Tag,
     TagLabel,
+    Tooltip,
     useColorModeValue
 } from '@chakra-ui/react'
 import ActionContainer from './theme-card-actions.component'
+import { ICourse } from '../../../pages/home'
 
-export default function ThemeCardComponent() {
+export default function ThemeCardComponent({title, color, tags, date, teacher}: Omit<ICourse, "id">) {
   return (
       <Flex
         bg={useColorModeValue('white', 'gray.800')}
@@ -25,29 +27,29 @@ export default function ThemeCardComponent() {
         p={2}
         position="relative">
 
-        <Flex h={48} bg={"#e3dbfa"} rounded="lg" flexDirection="column" justifyContent="space-between" p={2}>
+        <Flex h={48} bg={color} rounded="lg" flexDirection="column" justifyContent="space-between" p={2}>
             <Tag mt={2} size='lg' w="fit-content" borderRadius='full' colorScheme={useColorModeValue('gray', 'gray')}>
-                <TagLabel fontSize={12} fontWeight={600} color={useColorModeValue('gray', 'gray')}>18/09/2024</TagLabel>
+                <TagLabel fontSize={12} fontWeight={600} color={useColorModeValue('gray', 'gray')}>{date}</TagLabel>
             </Tag>
             <Box>
                 <Box mb={-2} fontSize={12} fontWeight={500} color={useColorModeValue('black', 'black')}>
-                    Prof. Eleonor
+                    Prof. {teacher}
                 </Box>
                 <Box fontSize={24} fontWeight={500} color={useColorModeValue('black', 'black')}>
-                    Ruby on Rails
+                    {title}
                 </Box>
             </Box>
 
             <Flex gap={1}>
-                <Tag size='sm' w="fit-content" pt={1} pb={1} borderRadius='full'>
-                    <TagLabel fontSize={12} fontWeight={600}>Docker</TagLabel>
-                </Tag>
-                <Tag size='sm' w="fit-content" pt={1} pb={1} borderRadius='full'>
-                    <TagLabel fontSize={12} fontWeight={600}>Server</TagLabel>
-                </Tag>
-                <Tag size='sm' w="fit-content" pt={1} pb={1} borderRadius='full'>
-                    <TagLabel fontSize={12} fontWeight={600}>SQL</TagLabel>
-                </Tag>
+                {tags.map((tag) =>
+                    <>
+                        <Tooltip label={tag}>
+                            <Tag size='sm' w="fit-content" pt={1} pb={1} borderRadius='full'>
+                                <TagLabel fontSize={12} fontWeight={600}>{tag}</TagLabel>
+                            </Tag>
+                        </Tooltip>
+                    </>
+                )}
             </Flex>
         </Flex>
         <Box mt={2} justifyContent="space-between">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,13 @@ import { useMemo, useState } from 'react'
 import ThemeCardComponent from '../components/molecules/theme-card/theme-card.component'
 import courses from '../data/courses.json'
 
-interface ICourse {
+export interface ICourse {
     id: number
     title: string
     tags: string[]
     color: string
+    date: string
+    teacher: string
 }
 
 function Home() {
@@ -19,8 +21,8 @@ function Home() {
     <>
         <Flex gap={5} w="full" justifyContent="center" h="fit-content" pt={25} pb={25}>
             {
-                filteredCourses.map(course => (
-                    <ThemeCardComponent />
+                filteredCourses.map(({id, title, color, tags, date, teacher}: ICourse) => (
+                    <ThemeCardComponent key={id} title={title} color={color} tags={tags} date={date} teacher={teacher}/>
                 ))
             }
         </Flex>
